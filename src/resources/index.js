a9bbc4924f15ade2c44bc9cb1eb3237d8c77299c
Jérémy Rigaldo <rigaldo.jeremy@gmail.com>
2018-10-03 22:20:09 +0200

    add news-list

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,5 +1,6 @@
 export function configure(config) {
   config.globalResources([
-    PLATFORM.moduleName('./value-converters/activity-type-to-route')
+    PLATFORM.moduleName('./value-converters/activity-type-to-route'),
+    PLATFORM.moduleName('./value-converters/date'),
   ]);
 }
